6f376500f93a87792bf8c9b7d2b3950f8e359091
Tim Orr <timorr@RES18LT298.local>
2019-11-19 08:30:14 -0500

    route additions

--- main.tf
+++ main.tf
@@ -24,3 +24,33 @@ resource "aws_ec2_transit_gateway_vpc_attachment_accepter" "this" {
   transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.this[0].id
   tags                          = merge(var.tags, map("Name", var.name))
 }
+
+# TGW Owner route table route to the local VPC CIDR
+resource "aws_route" "remote" {
+  provider = "aws.owner"
+  count    = var.create_tgw_attachment ? length(var.remote_route_tables) : 0
+
+  route_table_id         = var.remote_route_tables[count.index]
+  destination_cidr_block = var.vpc_cidr_local
+  transit_gateway_id     = var.transit_gateway_id
+}
+
+# Public route table route to the TGW for a remote CIDR
+resource "aws_route" "public" {
+  provider = "aws"
+  count    = var.create_tgw_attachment ? length(var.public_route_tables) : 0
+
+  route_table_id         = var.public_route_tables[count.index]
+  destination_cidr_block = var.vpc_cidr_remote
+  transit_gateway_id     = var.transit_gateway_id
+}
+
+# Private route table route to the TGW for a remote CIDR
+resource "aws_route" "private" {
+  provider = "aws"
+  count    = var.create_tgw_attachment ? length(var.private_route_tables) : 0
+
+  route_table_id         = var.private_route_tables[count.index]
+  destination_cidr_block = var.vpc_cidr_remote
+  transit_gateway_id     = var.transit_gateway_id
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+###########################
+## TGW Attachment Variables
+###########################
+
 variable "create_tgw_attachment" {
   description = "Controls whether to create the TGW attachment"
   type        = bool
@@ -38,4 +42,37 @@ variable "tags" {
   description = "A map of tags to apply to the TGW attachment"
   type        = map
   default     = {}
+}
+
+##################
+## Route Variables
+##################
+variable "vpc_cidr_remote" {
+  description = "The remote CIDR block of the VPC route"
+  type        = string
+  default     = null
+}
+
+variable "vpc_cidr_local" {
+  description = "The local CIDR block of the VPC route"
+  type        = string
+  default     = null
+}
+
+variable "private_route_tables" {
+  description = "List of IDs of local private route tables to route to the remote VPC CIDR"
+  type        = list(string)
+  default     = []
+}
+
+variable "public_route_tables" {
+  description = "List of IDs of local public route tables to route to the remote VPC CIDR"
+  type        = list(string)
+  default     = []
+}
+
+variable "remote_route_tables" {
+  description = "List of IDs of remote route tables to route to the local VPC CIDR"
+  type        = list(string)
+  default     = []
 }
