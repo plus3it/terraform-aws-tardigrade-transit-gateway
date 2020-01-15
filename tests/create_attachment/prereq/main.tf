78880f76e46779f11fd1151577ee665bb535c862
Loren Gordon <lorengordon@users.noreply.github.com>
2020-01-15 11:29:05 -0800

    Cleans up the create_attachment test

--- tests/create_attachment/prereq/main.tf
+++ tests/create_attachment/prereq/main.tf
@@ -8,8 +8,8 @@ provider aws {
 
 provider aws {
   region  = "us-east-1"
-  profile = "owner"
   alias   = "owner"
+  profile = "owner"
 }
 
 data "aws_caller_identity" "current" {}
@@ -22,8 +22,9 @@ resource "random_string" "this" {
 }
 
 resource "aws_ram_resource_share" "this" {
-  provider                  = aws.owner
-  name                      = "tardigrade-tgw-${random_string.this.result}"
+  provider = aws.owner
+
+  name                      = "tardigrade-tgw-${local.test_id}"
   allow_external_principals = true
 
   tags = {
@@ -32,22 +33,33 @@ resource "aws_ram_resource_share" "this" {
 }
 
 resource "aws_ec2_transit_gateway" "this" {
-  provider    = aws.owner
-  description = "tardigrade-tgw-${random_string.this.result}"
+  provider = aws.owner
+
+  description = "tardigrade-tgw-${local.test_id}"
 }
 
 resource "aws_ram_resource_association" "this" {
-  provider           = aws.owner
+  provider = aws.owner
+
   resource_arn       = aws_ec2_transit_gateway.this.arn
   resource_share_arn = aws_ram_resource_share.this.arn
 }
 
 resource "aws_ram_principal_association" "this" {
-  provider           = aws.owner
+  provider = aws.owner
+
   principal          = data.aws_caller_identity.current.account_id
   resource_share_arn = aws_ram_resource_share.this.arn
 }
 
+locals {
+  test_id = random_string.this.result
+}
+
 output "tgw_id" {
   value = aws_ec2_transit_gateway.this.id
+}
+
+output "test_id" {
+  value = local.test_id
 }
