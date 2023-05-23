15e15111fadf61cbb0a9a2b6e640b3363b65294a
Ehinome Ihenyen <ehi.ihenyen@gmail.com>
2023-05-23 11:30:10 -0400

    Terraform formatting

--- modules/vpc-accepter/main.tf
+++ modules/vpc-accepter/main.tf
@@ -14,26 +14,26 @@ resource "aws_ec2_transit_gateway_vpc_attachment_accepter" "this" {
 }
 
 resource "aws_ec2_transit_gateway_route_table_association" "this" {
-  count = var.transit_gateway_route_table_association != null && data.aws_ec2_transit_gateway_attachment.attachment.auto_accept_shared_attachments == "disabled" ? 1 : 0
+  count                          = var.transit_gateway_route_table_association != null && data.aws_ec2_transit_gateway_attachment.attachment.auto_accept_shared_attachments == "disabled" ? 1 : 0
 
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment_accepter.this.id
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment_accepter.this[0].id
   transit_gateway_route_table_id = var.transit_gateway_route_table_association.transit_gateway_route_table_id
 }
 
 resource "aws_ec2_transit_gateway_route_table_propagation" "this" {
-  for_each = { for route_table in var.transit_gateway_route_table_propagations : route_table.name => route_table }
+  for_each = var.transit_gateway_route_table_propagations
   count    = data.aws_ec2_transit_gateway_attachment.attachment.auto_accept_shared_attachments == "disabled" ? length(var.transit_gateway_route_table_propagations) : 0
 
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment_accepter.this.id
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment_accepter.this[0].id
   transit_gateway_route_table_id = each.value.transit_gateway_route_table_id
 }
 
 resource "aws_route" "this" {
-  for_each = { for route in var.vpc_routes : route.name => route }
+  for_each = var.vpc_routes
   count    = data.aws_ec2_transit_gateway_attachment.attachment.auto_accept_shared_attachments == "disabled" ? length(var.vpc_routes) : 0
 
   route_table_id              = each.value.route_table_id
   destination_cidr_block      = each.value.destination_cidr_block
   destination_ipv6_cidr_block = each.value.destination_ipv6_cidr_block
-  transit_gateway_id          = aws_ec2_transit_gateway_vpc_attachment_accepter.this.transit_gateway_id
-}
+  transit_gateway_id          = aws_ec2_transit_gateway_vpc_attachment_accepter.this[0].transit_gateway_id
+  }
